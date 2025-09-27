70eb793f9d5cb7e7aac076bed0482d3131804fb5
Tejas Dahiya <145849720+TejasDahiya09@users.noreply.github.com>
2025-09-27 17:34:42 +0530

    Delete connection.py

--- connection.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def get_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="sqluser",       # change if needed
-        password="password",  # change if needed
-        database="studentmanagementsystem"
-    )
